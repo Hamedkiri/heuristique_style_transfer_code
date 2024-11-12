4eef82adfefa0e38d43f4006f70d8f3bc73b7719
Ouattara <82286853+Hamedkiri@users.noreply.github.com>
2024-11-12 16:04:55 +0100

    reading readme

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# heuristique_style_transfer_code
- heuristique style transfer code
+
+# Heuristic Style Transfer Code
+
+## Installing Required Packages
+
+To install the required packages, create a Python virtual environment and then run the following command:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Testing the Models
+
+To test the models, you need to specify the hyperparameters file with the `--config_path` option to rebuild the model architecture, as well as the weights file with `--model_path`.
+
+The command-line options are similar for all three models and include the following parameters:
+
+- `--mode`: Specifies the type of test, with options such as `classification`, `tsne`, `tsne_interactive`, `camera`, `inference`, or for some models, `clustering` or `style_transfer`.
+- `--data`: Path to the data folder, which should contain the classes in a "test" subfolder (e.g., `dataset/test/fog rain snow sun`).
+- `--classes`: List of classes in alphabetical order (e.g., `--classes fog rain snow sun`).
+- `--num_samples`: Number of samples to use for testing.
+- `--save_dir`: Folder to save the results.
+
+An optional parameter, `--measure_time`, can be added in `camera` mode to measure the processing time per image accurately.
+
+### Example Testing Commands for Each Model
+
+- **Truncated ResNet50:**
+  ```bash
+  python test_RESNET50_Truncate.py --config_path checkpoints/Best_ResNet50_Truncated/best_model_fold_hyperparameters_all.json --model_path checkpoints/Best_ResNet50_Truncated/best_model_fold_all.pth --num_samples 12000 --mode camera --classes fog rain snow sun
+  ```
+
+- **Truncated ResNet50 + Gram Matrix + Attention:**
+  ```bash
+  python test_RESNET50_Truncate_gram_attention.py --data ./datasets --model_path checkpoints/Best_Resner50_Truncated_with_Attention/best_model_all.pth --config_path checkpoints/Best_Resner50_Truncated_with_Attention/best_performance_all.json --mode classification --num_samples 12000 --save_dir results/test_results_gram_attention_resnet50_convolution
+  ```
+
+- **Multi PatchGAN:**
+  ```bash
+  python test_Multi_PatchGAN.py --model_path checkpoints/Best_Multi_PatchGAN/best_model_all.pth --config_path checkpoints/Best_Multi_PatchGAN/best_hyperparameters_all.json --data ./datasets --num_samples 12000 --mode tsne_interactive
+  ```
+
+## Training the Models
+
+To retrain the models, use `--config_path` to specify the architecture and `--data` for the path to the training data, with classes in a "train" subfolder (e.g., `dataset/train/fog rain snow sun`). If you want to retrain from existing weights, add the `--model_path` option. The `--save_dir` parameter specifies where to save the model weights. Other options include `--epochs` to define the number of epochs and `--k_folds` for the number of cross-validation folds.
+
+### Example Training Commands for Each Model
+
+- **Truncated ResNet50:**
+  ```bash
+  python train_best_RESNET50_Truncate.py --data ./datasets --config_path checkpoints/Best_ResNet50_Truncated/best_model_fold_hyperparameters_all.json --model_path checkpoints/Best_ResNet50_Truncated/best_model_fold_all.pth --save_dir results/test_cancer_best --epochs 10 --k_folds 2
+  ```
+
+- **Truncated ResNet50 + Gram Matrix + Attention:**
+  ```bash
+  python train_best_RESNET50_Truncate_gram_attention.py --data ./datasets --model_path checkpoints/Best_Resner50_Truncated_with_Attention/best_model_all.pth --config_path checkpoints/Best_Resner50_Truncated_with_Attention/best_performance_all.json --epochs 10 --save_dir results/Models_resnet50_attention
+  ```
+
+- **Multi PatchGAN:**
+  ```bash
+  python train_best_Multi_PatchGAN.py --data ./datasets --model_path checkpoints/Best_Multi_PatchGAN/best_model_all.pth --config_path checkpoints/Best_Multi_PatchGAN/best_hyperparameters_all.json --save_dir results/Models_Multi_patchGAN --epochs 200 --k_folds 2
+  ```
+
+This `README.md` provides instructions on installing, testing, and training the models. Model weights are available at the following link: [Google Drive](https://drive.google.com/drive/folders/11Pllunglo-_XcZSI80WheTKOeqceW9II?usp=sharing).
+```
--- test_RESNET50_Truncate.py
+++ test_RESNET50_Truncate.py
@@ -13,8 +13,15 @@ import random
 
 import hdbscan
 from Models.Models_RESNET50_TRUNCATE import TruncatedMoCoV3, Classifier, CombinedModel
-from functions.functions_RESNET50_Truncate import load_best_model, compute_embeddings_with_paths, test, perform_tsne, run_camera, plot_tsne_interactive, list_available_cameras
-
+from functions.functions_RESNET50_Truncate import (
+    load_best_model,
+    compute_embeddings_with_paths,
+    test,
+    perform_tsne,
+    run_camera,
+    plot_tsne_interactive,
+    list_available_cameras
+)
 
 def main():
     parser = argparse.ArgumentParser(description='MoCo pour une tâche de classification spécifique')
@@ -30,19 +37,19 @@ def main():
     parser.add_argument('--visualize_gradcam', action='store_true', help='Visualiser Grad-CAM et l\'image avant transformation')
     parser.add_argument('--save_gradcam_images', action='store_true', help='Enregistrer les images Grad-CAM')
     parser.add_argument('--measure_time', action='store_true', help='Mesurer et enregistrer le temps moyen de traitement par image')
-    parser.add_argument('--mode', choices=['classifier', 'tsne', 'tsne_interactive', 'camera', 'inference', 'clustering'], default='classifier', help='Mode d\'opération')
+    parser.add_argument('--mode', choices=['classification', 'tsne', 'tsne_interactive', 'camera', 'inference', 'clustering'], default='classifier', help='Mode d\'opération')
     parser.add_argument('--colors', nargs='+', default=None, metavar='COLORS', help='Liste des couleurs pour t-SNE ou clustering')
     parser.add_argument('--clustering_class', type=str, help='Nom de la classe pour le clustering HDBSCAN')
     parser.add_argument('--min_cluster_size', type=int, nargs='+', default=[10, 15, 20], metavar='MIN_CLUSTER_SIZE', help='Liste des valeurs min_cluster_size pour HDBSCAN')
     parser.add_argument('--min_samples', type=int, nargs='+', default=[5, 10], metavar='MIN_SAMPLES', help='Liste des valeurs min_samples pour HDBSCAN')
     parser.add_argument('--kalman_filter', action='store_true', help='Appliquer un filtre de Kalman pour lisser les prédictions de la caméra')
-    parser.add_argument('--save_test_images', action='store_true', help='Sauvegarder les images d\'évaluation et de test avec prédictions et probabilités')  # Nouvelle option
+    parser.add_argument('--save_test_images', action='store_true', help='Sauvegarder les images d\'évaluation et de test avec prédictions et probabilités')
     parser.add_argument('--test_data', type=str, help="Chemin vers les données de test d'origine")
     parser.add_argument('--list_cameras', action='store_true', help='Lister les caméras disponibles')
     parser.add_argument('--camera_index', type=int, default=0, help='Index de la caméra à utiliser')
     parser.add_argument('--colormap', type=str, default='hot', help='Colormap pour les visualisations Grad-CAM (par exemple, hot, autumn, afmhot)')
-    parser.add_argument('--compute_auc', action='store_true', help='Calculer le score AUC pour le modèle')  # Nouvel argument
-
+    parser.add_argument('--compute_auc', action='store_true', help='Calculer le score AUC pour le modèle')
+    parser.add_argument('--classes', nargs='+', type=str, help='Liste des classes à utiliser lors de l\'utilisation du mode caméra')  # Nouvelle option
 
     args = parser.parse_args()
 
@@ -71,6 +78,11 @@ def main():
     if args.tensorboard:
         writer = SummaryWriter(log_dir=os.path.join(args.save_dir, 'TensorBoard'))
 
+    if args.list_cameras:
+        available_cameras = list_available_cameras()
+        print(f"Caméras disponibles : {available_cameras}")
+        return
+
     with open(args.config_path, 'r') as f:
         best_config = json.load(f)
 
@@ -81,7 +93,10 @@ def main():
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
-    if 'classes' in best_config:
+    # Déterminer les noms des classes
+    if args.classes:
+        class_names = args.classes
+    elif 'classes' in best_config:
         class_names = best_config['classes']
     elif args.data:
         dataset = datasets.ImageFolder(root=os.path.join(args.data, 'test'), transform=transform)
@@ -95,7 +110,10 @@ def main():
     classifier = Classifier(input_dim=256, num_classes=len(class_names))
     load_best_model(classifier, moco_model, args.model_path)
 
-    if args.data:
+    # Modes d'opération
+    if args.mode == 'classification':
+        if not args.data:
+            raise ValueError("Le chemin du dataset doit être spécifié pour le mode classification")
         dataset = datasets.ImageFolder(root=os.path.join(args.data, 'test'), transform=transform)
 
         if args.num_samples is not None:
@@ -106,106 +124,123 @@ def main():
 
         test_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
 
-        if args.mode == 'classifier':
-            test_accuracy, test_loss, precision, recall, f1, cm, auc = test(
-                classifier,
-                moco_model,
-                test_loader,
-                nn.CrossEntropyLoss().cuda(),
-                writer if args.tensorboard else None,
-                class_names,
-                args.save_dir,
-                transform,
-                args.prob_threshold,
-                args.visualize_gradcam,
-                args.save_gradcam_images,
-                args.measure_time,
-                args.save_test_images,
-                colormap=chosen_colormap,
-                compute_auc=args.compute_auc
-            )
-            print(
-                f"Précision du test: {test_accuracy}, Perte du test: {test_loss}, Précision: {precision}, Rappel: {recall}, Score F1: {f1}")
-            if auc is not None:
-                print(f"Score AUC: {auc}")
-        elif args.mode in ['tsne', 'tsne_interactive']:
-            all_embeddings, all_labels, img_paths = compute_embeddings_with_paths(moco_model, test_loader, 'cuda')
-            results = {'embeddings': all_embeddings.tolist(), 'labels': all_labels.tolist()}
-            output_path = os.path.join(args.save_dir, os.path.basename(args.model_path).replace('.pth', '_embeddings.json'))
-            os.makedirs(os.path.dirname(output_path), exist_ok=True)  # Assurez-vous que le répertoire existe
-            with open(output_path, 'w') as f:
-                json.dump(results, f)
-            if args.mode == 'tsne':
-                perform_tsne(all_embeddings, all_labels, class_names, args.colors, args.save_dir)
-            else:
-                plot_tsne_interactive(all_embeddings, all_labels, class_names, img_paths, args.colors, save_dir=args.save_dir)
-        elif args.mode == 'clustering':
-            all_embeddings, all_labels, img_paths = compute_embeddings_with_paths(moco_model, test_loader, 'cuda')
-            if not args.clustering_class:
-                raise ValueError("L'option --clustering_class doit être spécifiée pour le mode clustering")
-            class_index = class_names.index(args.clustering_class)
-            class_embeddings = all_embeddings[all_labels == class_index]
-            class_img_paths = [img_paths[i] for i in range(len(all_labels)) if all_labels[i] == class_index]
+        test_accuracy, test_loss, precision, recall, f1, cm, auc = test(
+            classifier,
+            moco_model,
+            test_loader,
+            nn.CrossEntropyLoss().cuda(),
+            writer if args.tensorboard else None,
+            class_names,
+            args.save_dir,
+            transform,
+            args.prob_threshold,
+            args.visualize_gradcam,
+            args.save_gradcam_images,
+            args.measure_time,
+            args.save_test_images,
+            colormap=chosen_colormap,
+            compute_auc=args.compute_auc
+        )
+        print(f"Précision du test: {test_accuracy}, Perte du test: {test_loss}, Précision: {precision}, Rappel: {recall}, Score F1: {f1}")
+        if auc is not None:
+            print(f"Score AUC: {auc}")
 
-            best_num_clusters = 0
-            best_cluster_labels = None
-            best_params = {}
+    elif args.mode in ['tsne', 'tsne_interactive']:
+        if not args.data:
+            raise ValueError("Le chemin du dataset doit être spécifié pour le mode t-SNE")
+        dataset = datasets.ImageFolder(root=os.path.join(args.data, 'test'), transform=transform)
 
-            for min_cluster_size in args.min_cluster_size:
-                for min_samples in args.min_samples:
-                    print(f"Testing HDBSCAN with min_cluster_size={min_cluster_size}, min_samples={min_samples}")
-                    clustering = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, min_samples=min_samples).fit(class_embeddings)
-                    cluster_labels = clustering.labels_
+        test_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
+        all_embeddings, all_labels, img_paths = compute_embeddings_with_paths(moco_model, test_loader, 'cuda')
+        results = {'embeddings': all_embeddings.tolist(), 'labels': all_labels.tolist()}
+        output_path = os.path.join(args.save_dir, os.path.basename(args.model_path).replace('.pth', '_embeddings.json'))
+        os.makedirs(os.path.dirname(output_path), exist_ok=True)
+        with open(output_path, 'w') as f:
+            json.dump(results, f)
+        if args.mode == 'tsne':
+            perform_tsne(all_embeddings, all_labels, class_names, args.colors, args.save_dir)
+        else:
+            plot_tsne_interactive(all_embeddings, all_labels, class_names, img_paths, args.colors, save_dir=args.save_dir)
 
-                    num_clusters = len(set(cluster_labels)) - (1 if -1 in cluster_labels else 0)
-                    print(f"Number of clusters found: {num_clusters}")
+    elif args.mode == 'clustering':
+        if not args.data:
+            raise ValueError("Le chemin du dataset doit être spécifié pour le mode clustering")
+        dataset = datasets.ImageFolder(root=os.path.join(args.data, 'test'), transform=transform)
 
-                    if num_clusters > best_num_clusters:
-                        best_num_clusters = num_clusters
-                        best_cluster_labels = cluster_labels
-                        best_params = {'min_cluster_size': min_cluster_size, 'min_samples': min_samples}
+        test_loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False, num_workers=4)
+        all_embeddings, all_labels, img_paths = compute_embeddings_with_paths(moco_model, test_loader, 'cuda')
+        if not args.clustering_class:
+            raise ValueError("L'option --clustering_class doit être spécifiée pour le mode clustering")
+        class_index = class_names.index(args.clustering_class)
+        class_embeddings = all_embeddings[all_labels == class_index]
+        class_img_paths = [img_paths[i] for i in range(len(all_labels)) if all_labels[i] == class_index]
 
-            if best_cluster_labels is None:
-                raise ValueError("No clusters found with the provided HDBSCAN parameters.")
+        best_num_clusters = 0
+        best_cluster_labels = None
+        best_params = {}
 
-            cluster_labels = best_cluster_labels
+        for min_cluster_size in args.min_cluster_size:
+            for min_samples in args.min_samples:
+                print(f"Testing HDBSCAN with min_cluster_size={min_cluster_size}, min_samples={min_samples}")
+                clustering = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, min_samples=min_samples).fit(class_embeddings)
+                cluster_labels = clustering.labels_
 
-            # Préparer les résultats du clustering
-            cluster_info = {}
-            unique_labels = set(cluster_labels)
-            for label in unique_labels:
-                indices = [i for i, lbl in enumerate(cluster_labels) if lbl == label]
-                cluster_info[str(label)] = {
-                    'num_images': len(indices),
-                    'img_paths': [class_img_paths[i] for i in indices]
-                }
+                num_clusters = len(set(cluster_labels)) - (1 if -1 in cluster_labels else 0)
+                print(f"Number of clusters found: {num_clusters}")
 
-            clustering_results = {
-                'num_clusters': best_num_clusters,
-                'clusters': cluster_info,
-                'best_params': best_params
+                if num_clusters > best_num_clusters:
+                    best_num_clusters = num_clusters
+                    best_cluster_labels = cluster_labels
+                    best_params = {'min_cluster_size': min_cluster_size, 'min_samples': min_samples}
+
+        if best_cluster_labels is None:
+            raise ValueError("No clusters found with the provided HDBSCAN parameters.")
+
+        cluster_labels = best_cluster_labels
+
+        # Préparer les résultats du clustering
+        cluster_info = {}
+        unique_labels = set(cluster_labels)
+        for label in unique_labels:
+            indices = [i for i, lbl in enumerate(cluster_labels) if lbl == label]
+            cluster_info[str(label)] = {
+                'num_images': len(indices),
+                'img_paths': [class_img_paths[i] for i in indices]
             }
 
-            clustering_output_path = os.path.join(args.save_dir, f'{args.clustering_class}_clustering_results.json')
-            with open(clustering_output_path, 'w') as f:
-                json.dump(clustering_results, f)
+        clustering_results = {
+            'num_clusters': best_num_clusters,
+            'clusters': cluster_info,
+            'best_params': best_params
+        }
 
-            print(f"Clustering results saved in '{clustering_output_path}' with parameters {best_params}")
+        clustering_output_path = os.path.join(args.save_dir, f'{args.clustering_class}_clustering_results.json')
+        with open(clustering_output_path, 'w') as f:
+            json.dump(clustering_results, f)
 
-            plot_tsne_interactive(class_embeddings, cluster_labels, [f'Cluster {i}' for i in range(best_num_clusters)] + ['Noise'], class_img_paths, colors=args.colors, num_clusters=best_num_clusters, save_dir=args.save_dir)
+        print(f"Clustering results saved in '{clustering_output_path}' with parameters {best_params}")
 
+        plot_tsne_interactive(class_embeddings, cluster_labels, [f'Cluster {i}' for i in range(best_num_clusters)] + ['Noise'], class_img_paths, colors=args.colors, num_clusters=best_num_clusters, save_dir=args.save_dir)
 
     elif args.mode == 'camera':
+        run_camera(
+            classifier,
+            moco_model,
+            transform,
+            class_names,
+            args.save_camera_video,
+            args.save_dir,
+            args.prob_threshold,
+            args.measure_time,
+            args.kalman_filter,
+            args.camera_index
+        )
 
-        run_camera(classifier, moco_model, transform, class_names, args.save_camera_video, args.save_dir,
-                   args.prob_threshold, args.measure_time, args.kalman_filter, args.camera_index)
+    else:
+        raise ValueError(f"Mode non reconnu: {args.mode}")
 
     if args.tensorboard:
         writer.close()
 
-    if args.list_cameras:
-        available_cameras = list_available_cameras()
-        print(f"Caméras disponibles : {available_cameras}")
-        return
 if __name__ == '__main__':
     main()
